c9471509cb9e14601bf48637af8ac10ec6bdbc56
unknown <bhubankhatiwada22@gmail.com>
2022-07-06 08:56:06 +0545

    code

--- lib/features/attendance/views/attendance_view.dart
+++ lib/features/attendance/views/attendance_view.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tiktaktoe/helpers/image_picker_helper/image_picker.dart';
 
 class AttendanceView extends StatefulWidget {
   const AttendanceView({Key? key}) : super(key: key);
@@ -12,7 +16,50 @@ class _AttendanceViewState extends State<AttendanceView> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-      title: Text("Attendance"),
-    ));
+          automaticallyImplyLeading: false,
+          title: Text("Attendance"),
+        ),
+        body: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                showModalBottomSheet(
+                    context: context,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                    )),
+                    builder: (context) {
+                      return SizedBox(
+                        // height: 220,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            ListTile(
+                              onTap: (() async {
+                                ImagePickerHelper()
+                                    .pickImage(ImageSource.camera);
+                              }),
+                              leading: const Icon(Icons.camera),
+                              title: const Text("Camera"),
+                            ),
+                            ListTile(
+                              onTap: () async {
+                                ImagePickerHelper()
+                                    .pickImage(ImageSource.gallery);
+                              },
+                              leading: const Icon(Icons.browse_gallery),
+                              title: const Text("Gallary"),
+                            )
+                          ],
+                        ),
+                      );
+                    });
+              },
+              child: const Text("Pick an image"),
+            ),
+          ],
+        ));
   }
 }
